833a01b8fb1739f99a60462477289c6b209fa854
Kayla Luebbert <luebbertkayla@gmail.com>
2021-04-08 02:09:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,8 +15,8 @@ class Scraper
     
     page.css("div.student-card").each do |student|
       students << {
-        :name => student.css("h4.student-name"),
-        :location => student.css("p.student-location"),
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
         :profile_url => student.children[1].attributes["href"].value
       }
      end
