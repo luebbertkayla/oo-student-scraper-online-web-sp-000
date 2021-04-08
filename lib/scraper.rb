ea821a2a0386eaa822ebb939e531336e7e283e86
Kayla Luebbert <luebbertkayla@gmail.com>
2021-04-08 02:06:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,24 +4,25 @@ require 'nokogiri'
 
 class Scraper
   attr_accessor :student
-  
   @@all = []
-  
+
   def self.scrape_index_page(index_url)
     html = open(index_url)
+
     page = Nokogiri::HTML(html)
-    
+
     students = []
-    
+
     page.css("div.student-card").each do |student|
+
       students << {
-        :name => student.css("h4.student-name"),
-        :location => student.css("p.student-location"),
-        :profile_url => student.children[1].attributes["href"].value
+         :name => student.css("h4.student-name").text,
+         :location => student.css("p.student-location").text,
+         :profile_url => student.children[1].attributes["href"].value
       }
-     end
-     students
-  end 
+    end
+    students
+  end
 
   def self.scrape_profile_page(profile_url)
     
