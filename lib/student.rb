82f8f6fae3ae2a467b8f0a9014e916bcb5f2d555
Kayla Luebbert <luebbertkayla@gmail.com>
2021-04-08 02:37:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      
+      student = self.
   end
 
   def add_student_attributes(attributes_hash)
